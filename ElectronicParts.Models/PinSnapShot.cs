06326f4f7f871048c5c459bfc05c0f4a2de06ead
PeterHelf <106546@fhwn.ac.at>
2019-01-18 09:11:12 +0100

    StyleCop Configuration + SnapShots

--- ElectronicParts.Models/PinSnapShot.cs
+++ ElectronicParts.Models/PinSnapShot.cs
@@ -1,24 +1,47 @@
-﻿using Shared;
-using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// ***********************************************************************
+// Assembly         : ElectronicParts.Models
+// Author           : Peter Helf
+// ***********************************************************************
+// <copyright file="PinSnapShot.cs" company="FHWN">
+//     Copyright ©  2019
+// </copyright>
+// <summary>Represents the PinSnapShot class of the ElectronicParts programm</summary>
+// ***********************************************************************
 
 namespace ElectronicParts.Models
 {
+    using System;
+    using System.Drawing;
+    using Shared;
+
+    /// <summary>
+    /// Represents the PinSnapShot class of the ElectronicParts program.
+    /// This class is used to create serializable instances of the PinViewModel class.
+    /// </summary>
     [Serializable]
     public class PinSnapShot
     {
-        public IPin Pin { get; set; }
-
-        public Point Position { get; set; }
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PinSnapShot"/> class.
+        /// </summary>
+        /// <param name="pin">The pin which will be serialized.</param>
+        /// <param name="point">The position of the pin.</param>
         public PinSnapShot(IPin pin, Point point)
         {
             this.Pin = pin;
             this.Position = point;
         }
+
+        /// <summary>
+        /// Gets the pin which will be serialized.
+        /// </summary>
+        /// <value>The pin which will be serialized.</value>
+        public IPin Pin { get; }
+
+        /// <summary>
+        /// Gets the position of the pin.
+        /// </summary>
+        /// <value>The position of the pin.</value>
+        public Point Position { get; }
     }
 }
--- ElectronicParts.Services/Implementations/ConfigurationService.cs
+++ ElectronicParts.Services/Implementations/ConfigurationService.cs
@@ -1,28 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Json;
-using System.Text;
-using System.Threading.Tasks;
-using ElectronicParts.Services.Interfaces;
-using Microsoft.Extensions.Configuration;
+﻿// ***********************************************************************
+// Assembly         : ElectronicParts.Services
+// Author           : Peter Helf
+// ***********************************************************************
+// <copyright file="ConfigurationService.cs" company="FHWN">
+//     Copyright ©  2019
+// </copyright>
+// <summary>Represents the ConfigurationService class of the ElectronicParts programm</summary>
+// ***********************************************************************
 
 namespace ElectronicParts.Services.Implementations
 {
+    using System.IO;
+    using System.Reflection;
+    using System.Runtime.Serialization.Json;
+    using ElectronicParts.Services.Interfaces;
+    using Microsoft.Extensions.Configuration;
+
+    /// <summary>
+    /// Represents the ConfigurationService class of the ElectronicParts program.
+    /// </summary>
     public class ConfigurationService : IConfigurationService
     {
-        public Configuration Configuration { get; private set; }
-
+        /// <summary>
+        /// The configurations which are read from a file.
+        /// </summary>
         private IConfiguration configuration;
-
+        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConfigurationService"/> class.
+        /// </summary>
         public ConfigurationService()
         {
             this.SetupConfiguration();
         }
 
+        /// <summary>
+        /// Gets the configurations which include all configurations needed in other classes of the program.
+        /// </summary>
+        /// <value>The Configuration which contains all needed configurations.</value>
+        public Configuration Configuration { get; private set; }
+
+        /// <summary>
+        /// Saves the configuration to a file.
+        /// </summary>
+        public void SaveConfiguration()
+        {
+            using (FileStream fileStream = new FileStream(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\appsettings.json", FileMode.OpenOrCreate))
+            {
+                DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(Configuration));
+                ser.WriteObject(fileStream, this.Configuration);
+            }
+        }
+
+        /// <summary>
+        /// Sets up the configuration by first reading it out of a file.
+        /// </summary>
         private void SetupConfiguration()
         {
             try
@@ -38,15 +70,6 @@ namespace ElectronicParts.Services.Implementations
             {
                 this.Configuration = new Configuration();
             }
-        }
-
-        public void SaveConfiguration()
-        {
-            using (FileStream fileStream = new FileStream(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\appsettings.json", FileMode.OpenOrCreate))
-            {
-                DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(Configuration));
-                ser.WriteObject(fileStream, this.Configuration);
-            }
-        }
+        }       
     }
 }
