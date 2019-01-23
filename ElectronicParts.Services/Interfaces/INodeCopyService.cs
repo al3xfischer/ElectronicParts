861b59b5327774c58fa7f7124e3b4e4d58a790b1
Kevin Janisch <106290@fhwn.ac.at>
2019-01-23 10:41:27 +0100

    added nodecopyservice

---
+++ ElectronicParts.Services/Interfaces/INodeCopyService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ElectronicParts.Models;
+using Shared;
+
+namespace ElectronicParts.Services.Interfaces
+{
+    public interface INodeCopyService
+    {
+        IEnumerable<Connector> CopiedConnectors { get; }
+        IEnumerable<IDisplayableNode> CopiedNodes { get; }
+
+        Task CopyTaskAwaiter();
+        void InitializeCopyProcess(IEnumerable<IDisplayableNode> nodes, IEnumerable<Connector> connectors);
+        bool TryBeginCopyTask();
+    }
+}
