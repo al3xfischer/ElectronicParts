557f44eb50e2689b62e357b06336fbb54d1e69e2
Kevin Janisch <106290@fhwn.ac.at>
2019-01-23 20:21:29 +0100

    added some line intelligence

--- ElectronicParts/Views/Connection.xaml.cs
+++ ElectronicParts/Views/Connection.xaml.cs
@@ -10,7 +10,13 @@
 
 namespace ElectronicParts.Views
 {
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Windows;
     using System.Windows.Controls;
+    using System.Windows.Media;
+    using System.Windows.Shapes;
 
     /// <summary>
     /// Interaction logic for Connection.
@@ -23,6 +29,36 @@ namespace ElectronicParts.Views
         public Connection()
         {
             this.InitializeComponent();
+        }
+
+        private void UserControl_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            //DrawingGroup drawingGroup = VisualTreeHelper.GetDrawing(sender as Visual);
+
+
+            //HitTestResult hitTestResult = VisualTreeHelper.HitTest(sender as Visual, e.GetPosition(this));
+            //var path = hitTestResult.VisualHit as Path;
+            //if (path == null)
+            //    return;
+
+            //// 2. Iterate through geometries of the Path and hit test each one
+            ////    to find a line to delete
+
+            //var geometryGroup = path.Data;
+            //if (geometryGroup == null)
+            //    return;
+
+            //var geometries = geometryGroup as PathGeometry;
+            //PathGeometry.Parse()
+            //var segments = geometries.Figures[0];
+            //var first = segments.Segments[0];
+            //VisualTreeHelper.GetDrawing(first);
+            //Point point = e.GetPosition(path);
+            //var pen = new Pen(path.Stroke, path.StrokeThickness);
+
+
+            // 3. Delete link
+
         }
     }
 }
