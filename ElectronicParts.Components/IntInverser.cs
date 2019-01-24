2c981b19b3fc261045f82c212dab2615d0dde018
PeterHelf <106546@fhwn.ac.at>
2019-01-24 10:17:49 +0100

    adding new component

---
+++ ElectronicParts.Components/IntInverser.cs
@@ -0,0 +1,49 @@
+﻿using Shared;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectronicParts.Components
+{
+    [Serializable]
+    public class IntInverser : IDisplayableNode
+    {
+        private DateTime lastChange;
+
+        public IntInverser()
+        {
+            this.Inputs = new List<IPin>() { new Pin<bool>(), new Pin<int>() };
+
+            this.Outputs = new List<IPin>() { new Pin<int>() };
+        }
+        public ICollection<IPin> Inputs { get; }
+
+        public ICollection<IPin> Outputs { get; }
+
+        public string Label => "IntInverser";
+
+        public string Description => "Inverses the incoming integer if the incoming boolean is true.";
+
+        public NodeType Type => NodeType.Logic;
+
+        public Bitmap Picture => Properties.Resources.Timer;
+
+        public event EventHandler PictureChanged;
+
+        public void Activate()
+        {
+            return;
+        }
+
+        public void Execute()
+        {
+            if ((bool)this.Inputs.ElementAt(0).Value.Current)
+            {
+
+            }
+        }
+    }
+}
