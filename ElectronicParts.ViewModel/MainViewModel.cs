c397884962a797e95bcf2c0baf8732684c4c8a0c
roman <roman_jahn@hotmail.com>
2019-01-24 13:18:31 +0100

    CompHandbook

--- ElectronicParts.ViewModel/MainViewModel.cs
+++ ElectronicParts.ViewModel/MainViewModel.cs
@@ -15,6 +15,7 @@ namespace ElectronicParts.ViewModels
     using System.Collections.ObjectModel;
     using System.Diagnostics;
     using System.Linq;
+    using System.Reflection;
     using System.Runtime.Serialization;
     using System.Threading.Tasks;
     using System.Timers;
@@ -695,6 +696,13 @@ namespace ElectronicParts.ViewModels
             }, 
             arg => !this.executionService.IsEnabled);
 
+            this.HowToCommand = new RelayCommand(
+                arg =>
+            {
+                var path = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                Process.Start(System.IO.Path.Combine(path, @"Resources\ComponentsHandbook.pdf"));
+            });
+
             this.Nodes = new ObservableCollection<NodeViewModel>();
             this.PreviewLines = new ObservableCollection<PreviewLineViewModel>() { new PreviewLineViewModel() };
             this.AvailableNodes = new ObservableCollection<NodeViewModel>();
@@ -1073,6 +1081,12 @@ namespace ElectronicParts.ViewModels
         /// </summary>
         /// <value>The add output pins command.</value>
         public ICommand AddOutputPinsCommand { get; }
+
+        /// <summary>
+        /// Gets the command to open the HowTo pdf.
+        /// </summary>
+        /// <value>The command to open the HowTo pdf.</value>
+        public ICommand HowToCommand { get; }
 
         /// <summary>
         /// Gets or sets the amount of executions per second. 
