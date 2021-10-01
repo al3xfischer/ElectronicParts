2394a10c8b87c02d456e07f3f3179993836b2f33
PaulAbloescher <abloescherpaul@gmail.com>
2021-10-01 14:58:06 +0200

    Cleanup class and logic.

--- ElectronicParts.ViewModel/Commands/RelayCommand.cs
+++ ElectronicParts.ViewModel/Commands/RelayCommand.cs
@@ -8,28 +8,28 @@
 // <summary>Represents the RelayCommand class of the ElectronicParts programm</summary>
 // ***********************************************************************
 
+using System;
+using System.Windows.Input;
+
 namespace ElectronicParts.ViewModels.Commands
 {
-    using System;
-    using System.Windows.Input;
-
     /// <summary>
-    /// Represents the RelayCommand class of the ElectronicParts program.
+    ///     Represents the RelayCommand class of the ElectronicParts program.
     /// </summary>
     public class RelayCommand : ICommand
     {
         /// <summary>
-        /// The action that is executed when the command is executed.
+        ///     The action that is executed when the command is executed.
         /// </summary>
         private readonly Action<object> action;
 
         /// <summary>
-        /// A predicate which indicates whether the command can execute or not.
+        ///     A predicate which indicates whether the command can execute or not.
         /// </summary>
         private readonly Predicate<object> canExecute;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="RelayCommand"/> class.
+        ///     Initializes a new instance of the <see cref="RelayCommand" /> class.
         /// </summary>
         /// <param name="action">The action that is executed when the command is executed.</param>
         /// <param name="canExecute">A predicate which indicates whether the command can execute or not.</param>
@@ -40,38 +40,32 @@ namespace ElectronicParts.ViewModels.Commands
         }
 
         /// <summary>
-        /// A event that is fired when the <see cref="CanExecute(object)"/> value changes.
+        ///     A event that is fired when the <see cref="CanExecute(object)" /> value changes.
         /// </summary>
         public event EventHandler CanExecuteChanged
         {
-            add
-            {
-                CommandManager.RequerySuggested += value;
-            }
+            add => CommandManager.RequerySuggested += value;
 
-            remove
-            {
-                CommandManager.RequerySuggested -= value;
-            }
+            remove => CommandManager.RequerySuggested -= value;
         }
 
         /// <summary>
-        /// Indicates whether the command can execute or not.
+        ///     Indicates whether the command can execute or not.
         /// </summary>
         /// <param name="parameter">A value used to decide if the command can execute or not.</param>
         /// <returns>A value indicating whether the command can execute or not.</returns>
         public bool CanExecute(object parameter = null)
         {
-            return this.canExecute?.Invoke(parameter) ?? true;
+            return canExecute?.Invoke(parameter) ?? true;
         }
 
         /// <summary>
-        /// Executes the previously defined action.
+        ///     Executes the previously defined action.
         /// </summary>
         /// <param name="parameter">The parameter used for the execution of the previously defined action.</param>
         public void Execute(object parameter)
         {
-            this.action(parameter);
+            action(parameter);
         }
     }
 }
